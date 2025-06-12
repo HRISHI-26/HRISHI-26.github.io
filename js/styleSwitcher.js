35e35b8f2209fc3ce755d3214d3ecb6971360115
HRISHI-26 <hrishiwynd26@gmail.com>
2025-06-12 07:51:23 +0530

    Portfolio changed to Certifications | Added default dark mode

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -2,8 +2,7 @@
 const links = document.querySelectorAll('.alternate-style'),
     totalLinks = links.length;
 
-function setActiveStyle(color)
-{
+function setActiveStyle(color) {
     for (let i = 0; i < totalLinks; i++) {
         if (color === links[i].getAttribute('title')) {
             links[i].removeAttribute('disabled');
@@ -14,19 +13,37 @@ function setActiveStyle(color)
 }
 
 // body skin
-
+// Apply light mode on page load as default
 const bodySkin = document.querySelectorAll('.body-skin'),
     totalBodySkin = bodySkin.length;
 
 for (let i = 0; i < totalBodySkin; i++) {
     bodySkin[i].addEventListener('change', function(){
-        if(this.value === 'dark'){
+        if(this.value !== 'dark'){
             document.body.className = 'dark';
         } else {
             document.body.className = '';
         }
     });
 }
+
+// ##############################################################
+
+// Apply dark mode on page load as default 
+// document.body.classList.add('dark');
+
+// // Listen to radio button changes
+// document.querySelectorAll('.body-skin').forEach((input) => {
+//     input.addEventListener('change', () => {
+//         if (input.value === 'dark') {
+//             document.body.classList.add('dark');
+//         } else {
+//             document.body.classList.remove('dark');
+//         }
+//     });
+// });
+
+// #############################################################
 
 document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
     document.querySelector('.style-switcher').classList.toggle('open');
